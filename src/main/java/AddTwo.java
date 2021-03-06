4acbf413bd08a39254d616e4f2b502c46a53c3d7
b7043159 <b7043159@newcastle.ac.uk>
2021-03-06 22:53:40 +0000

    Setup

---
+++ src/main/java/AddTwo.java
@@ -0,0 +1,9 @@
+public class AddTwo {
+    public int addTwo(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public String concStrings(String s1, String s2) {
+        return s1 + s2;
+    }
+}
---
+++ src/test/java/AddTwoTests.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AddTwoTests {
+
+    @Test
+    public void test1() {
+        System.out.println("Success");
+    }
+
+    @Test
+    public void testAdd() {
+        int expected = 5;
+        Assert.assertEquals(expected, new AddTwo().addTwo(5, 0));
+    }
+}
